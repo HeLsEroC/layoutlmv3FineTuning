7275994086eb77ef1d5e1cbfd52b598dd19a0d15
Wissem Marzouki <75568936+HeLsEroC@users.noreply.github.com>
2022-10-16 18:57:01 +0100

    load onnx model

--- Layoutlmv3_inference/utils.py
+++ Layoutlmv3_inference/utils.py
@@ -1,5 +1,37 @@
 import numpy as np
 from transformers import AutoModelForTokenClassification, AutoProcessor
+from optimum.onnxruntime import ORTModelForTokenClassification
+from typing import Optional
+from transformers.modeling_outputs import TokenClassifierOutput
+
+
+
+# input_ids', 'attention_mask', 'bbox', 'pixel_values'
+class ORTModelForTokenClassificationLM(ORTModelForTokenClassification):
+  def forward(
+      self,
+      input_ids=None,
+      attention_mask=None,
+      token_type_ids=None,
+      pixel_values=None,
+      bbox=None,
+      **kwargs,
+  ):
+      # converts pytorch inputs into numpy inputs for onnx
+      onnx_inputs = {
+          "input_ids": input_ids.cpu().detach().numpy(),
+          "attention_mask": attention_mask.cpu().detach().numpy(),
+          "pixel_values": pixel_values.cpu().detach().numpy(),
+          "bbox": bbox.cpu().detach().numpy(),
+      }
+      if token_type_ids is not None:
+          onnx_inputs["token_type_ids"] = token_type_ids.cpu().detach().numpy()
+      # run inference
+      outputs = self.model.run(None, onnx_inputs)
+      logits = torch.from_numpy(outputs[self.model_outputs["logits"]]).to(self.device)
+      # converts output to namedtuple for pipelines post-processing
+      return TokenClassifierOutput(logits=logits)
+
 
 def normalize_box(bbox, width, height):
     return [
@@ -40,9 +72,8 @@ def image_label_2_color(annotation):
   else:
     raise ValueError('please use "output" as annotation key')
 
-def load_model(model_path):
-    model = AutoModelForTokenClassification.from_pretrained(model_path)
-    return model
+def load_model(model_path, file_name="model_quantized.onnx"):
+    return ORTModelForTokenClassificationLM.from_pretrained(model_path, file_name=file_name)
 
 def load_processor():
     processor = AutoProcessor.from_pretrained(
